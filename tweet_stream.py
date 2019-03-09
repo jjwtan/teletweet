ed2ab89bbb026e642c3e6828ffc3f45faf092117
Jeremy Tan <jjwtan@gmail.com>
2019-03-10 01:20:13 +0800

    added user to review

--- tweet_stream.py
+++ tweet_stream.py
@@ -19,6 +19,7 @@ class MyStreamListener(tweepy.StreamListener):
     def process_json(self, json_string):
         extended_tweet = json.loads(json_string)
         follower_count = extended_tweet["user"]["followers_count"]
+        user = extended_tweet["user"]["screen_name"]
 
         original_tweet = extended_tweet
         if "retweeted_status" in extended_tweet: # capture the original tweet informtion instead of retweeter
@@ -29,7 +30,7 @@ class MyStreamListener(tweepy.StreamListener):
                 
                 media = original_tweet["entities"]["media"][0]
                 original_follower_count = original_tweet["user"]["followers_count"]
-                user = original_tweet["user"]["screen_name"]
+                ori_user = original_tweet["user"]["screen_name"]
                 tweet_id = media["id_str"]
                 url = media["expanded_url"]
 
@@ -40,7 +41,7 @@ class MyStreamListener(tweepy.StreamListener):
                         logger.info("sent %s > %s" % (url, send_mess(url)))
                         persist_tweet(original_tweet)
                     else:
-                        message = "@%s\n%s/%s\n%s" % (user, follower_count, original_follower_count, url)
+                        message = "@%s/@%s\n%s/%s\n%s" % (user, ori_user, follower_count, original_follower_count, url)
                         logger.info("to review %s > %s" % (url, send_review(message)))
         except Exception as e:
             logging.error("oh no %s", str(e))
